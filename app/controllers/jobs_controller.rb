24f34120119111a6c555c34ef6152a9e0f58dde9
David Guerette <david.guerette@gmail.com>
2014-06-17 22:53:56 -0400

    Added jobs#new page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,4 +8,27 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def new
+    @job = Job.new
+    @companies = Company.a
+  end
+
+  def create
+    @job = Job.new(user_params)
+    if @job.save
+      flash[:notice] = 'Saved!'
+      redirect_to job_path(@job)
+    else
+      flash[:notice] = 'Invalid input'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:job).permit(:title, :description, :company_id)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Muenster::Application.routes.draw do
 
-  resources :jobs, only: [:index, :show]
+  resources :jobs, only: [:index, :show, :new, :create]
   resources :companies, only: [:index, :show, :create, :new, :edit, :update]
 end
